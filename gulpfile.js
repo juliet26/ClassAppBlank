94cb9e633700b5be9a9448416366ea2dbe28f060
juliet26 <36687161+juliet26@users.noreply.github.com>
2018-05-10 20:36:08 -0500

    Nuevo
    estilos a platform android-ios

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -11,7 +12,13 @@ gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
+    .pipe(sass({
+		outputstyle:'extended', //compresed, compact
+		sourceComments: false
+	}))
+	.pipe(autoprefixer({
+	browsers: ['last 2 versions', 'firefox>=6']
+	}))
     .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
     .pipe(cleanCss({
